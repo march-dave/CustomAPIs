fb688912f0ccbca6595c26837de06291c41851a5
march-dave <ysangy@gmail.com>
2016-04-19 17:46:32 -0700

    node server init

--- math/index.js
+++ math/index.js
@@ -2,13 +2,37 @@
 
 module.exports = function(params, res) {
 
+  console.log('math index');
+
   var operation = params.shift();  // math, pow, etc.
 
+
+
   switch(operation) {
-    case 'sum':
-      var sum = require('./sum')(params);
-      res.write(`${sum}`);
+    case 'square':
+      var num = params[0];
+      var square = Math.pow(num, 2);
+      res.write(`${square}`);
       break;
+    case 'cube':
+      var num = params[0];
+      var cube = Math.pow(num, 3);
+      res.write(`${cube}`);
+      break;
+    case 'pow':
+      var base = params[0];
+      var exponent = params[1];
+      var pow = Math.pow(base, exponent);
+      res.write(`${pow}`);
+      break;
+    case 'sum':
+      var sum = params.reduce((acc, num) => {
+        return acc + parseInt(num);
+      }, 0);
+      res.write(`${sum}`)
+      break;
+    default:
+      res.write('Default.');
   }
   res.end('\n');
 };
--- server.js
+++ server.js
@@ -3,19 +3,49 @@
 const PORT = 3000;
 
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer((req, res) => {
   var params = req.url.split('/');
   params.shift(); // throwing out the empty string
-  var task = params.shift();
+  var resource = params.shift().toLowerCase();
 
-  switch(task) {
+  if(req.url === '/') { // send index.html
+    var data = fs.readFileSync('./index.html');
+    console.log('data', data);
+    res.end(data.toString());
+    return;
+  }
+
+  if(req.url === '/main') { // send index.html
+    var data = fs.readFileSync('./main.js');
+    console.log('data', data.toString());
+    res.end(data.toString());
+    return;
+  }
+
+
+  switch(resource) {
     case 'math': require('./math')(params, res); break;
     // case 'str': require('./str')(params, res); break;
 
+    case '':
+      // url is '/', serve
+      var data = fs.readFileSync('./index.html');
+      res.end( data.toString() );
+      break;
+
     default:
-      res.write('Default.');
-      res.end('\n');
+      // check if they're looking for a file
+      fs.readFile(`./${resource}` , (err, data) => {
+          if (err) {
+            res.statusCode = 404;
+            res.write('Not Found.');
+            res.end('\n');
+          } else { // file found
+            res.end(data.toString());
+          }
+      });
   }
 });
 
